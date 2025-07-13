08bb85fe5f083a1def588871b9caadddbcdbd0dc
Per Mortensen <per@permortensen.com>
2025-07-13 09:59:57 +0200

    Update dependencies

--- src/path.ts
+++ src/path.ts
@@ -7,11 +7,11 @@ const isNodeAvailable = typeof nodePath?.basename === "function";
  */
 export const basename = isNodeAvailable
 	? nodePath.basename
-	: (path: string) => path.match(/([^/]+)\/?$/)?.at(1) || path;
+	: (path: string) => path.match(/([^/]+)\/?$/)?.[1] || path;
 
 /**
  * Wrapper for Node's path.dirname with a fallback for mobile devices.
  */
 export const dirname = isNodeAvailable
 	? nodePath.dirname
-	: (path: string) => path.match(/^(.+)\/.+/)?.at(1) || ".";
+	: (path: string) => path.match(/^(.+)\/.+/)?.[1] || ".";
