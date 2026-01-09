d4ebcfaf6f4cc56a2fd118819d1c12976e10c6b5
Per Mortensen <per@permortensen.com>
2026-01-09 22:36:01 +0100

    Improve readability of e2e tests

--- e2e/trash-explorer.spec.ts
+++ e2e/trash-explorer.spec.ts
@@ -1,4 +1,9 @@
-import test, { expect, type ElectronApplication } from "@playwright/test";
+import test, {
+	expect,
+	type ElectronApplication,
+	type Locator,
+	type Page,
+} from "@playwright/test";
 import fs from "node:fs/promises";
 import path from "node:path";
 import { _electron as electron } from "playwright";
@@ -7,6 +12,7 @@ const appPath = path.resolve("./.obsidian-unpacked/main.js");
 const vaultPath = path.resolve("./e2e-vault");
 
 let app: ElectronApplication;
+let page: Page;
 
 test.beforeEach(async () => {
 	// Restore .trash folder from template
@@ -27,6 +33,8 @@ test.beforeEach(async () => {
 			`obsidian://open?path=${encodeURIComponent(vaultPath)}`,
 		],
 	});
+
+	page = await app.firstWindow();
 });
 
 test.afterEach(async () => {
@@ -34,97 +42,76 @@ test.afterEach(async () => {
 });
 
 test("can open trash explorer view from ribbon", async () => {
-	const window = await app.firstWindow();
-	const trashExplorerLeaf = window.locator(
-		".workspace-leaf-content[data-type=trash-explorer]"
-	);
+	const trashView = getTrashView(page);
+	const filesView = getFilesView(page);
 
-	await window.getByLabel("Files", { exact: true }).click();
-	await expect(trashExplorerLeaf).not.toBeVisible();
+	await openFilesView(page);
+	await expect(trashView).not.toBeVisible();
+	await expect(filesView).toBeVisible();
 
-	await window.getByLabel("Open trash explorer", { exact: true }).click();
-	await expect(trashExplorerLeaf).toBeVisible();
+	await openTrashView(page);
+	await expect(trashView).toBeVisible();
+	await expect(filesView).not.toBeVisible();
 });
 
 test("can empty the trash from command palette", async () => {
-	const window = await app.firstWindow();
-	await window.getByLabel("Open trash explorer", { exact: true }).click();
+	const view = await openTrashView(page);
 
-	await expect(window.locator(".trash-item")).toHaveCount(5);
+	const trashItems = getTrashItems(view);
+	await expect(trashItems).toHaveCount(5);
 
 	// Empty trash through the command palette
-	await window.getByLabel("Open command palette", { exact: true }).click();
-	await window.locator("input.prompt-input").pressSequentially("empty trash");
-	await expect(window.locator(".suggestion-item")).toHaveCount(1);
-	await window.locator(".suggestion-item").click();
-	await window.locator("button.mod-warning").getByText("Empty trash").click();
+	await page.getByLabel("Open command palette", { exact: true }).click();
+	await page.locator("input.prompt-input").pressSequentially("empty trash");
+	await expect(page.locator(".suggestion-item")).toHaveCount(1);
+	await page.locator(".suggestion-item").click();
+	await page.locator("button.mod-warning").getByText("Empty trash").click();
 
-	await expect(window.locator(".notice")).toHaveText("Emptied the trash");
-	await expect(window.locator(".trash-item")).toHaveCount(0);
+	await expect(page.locator(".notice")).toHaveText("Emptied the trash");
+	await expect(trashItems).toHaveCount(0);
 });
 
 test("can restore items from trash explorer view", async () => {
-	const window = await app.firstWindow();
-	const trashExplorerLeaf = window.locator(
-		".workspace-leaf-content[data-type=trash-explorer]"
-	);
-
-	await window.getByLabel("Open trash explorer", { exact: true }).click();
-	await expect(trashExplorerLeaf).toBeVisible();
+	const view = await openTrashView(page);
 
 	const trashItemName = "Recipes";
-	const trashItem = trashExplorerLeaf.locator(".trash-item", {
-		hasText: trashItemName,
-	});
+	const trashItem = getTrashItems(view).filter({ hasText: trashItemName });
 	await expect(trashItem).toContainText("2 files, 0 folders");
+
 	await trashItem.getByLabel("Restore").click();
 	await expect(trashItem).not.toBeVisible();
 
-	await window.getByLabel("Files", { exact: true }).click();
-	await expect(
-		window.locator(
-			".workspace-leaf-content[data-type=file-explorer] .nav-folder-title-content",
-			{ hasText: trashItemName }
-		)
-	).toBeVisible();
+	const filesView = await openFilesView(page);
+	const restoredItem = filesView.locator(".nav-folder-title-content", {
+		hasText: trashItemName,
+	});
+	await expect(restoredItem).toBeVisible();
 });
 
 test("can delete items from trash explorer view", async () => {
-	const window = await app.firstWindow();
-	const trashExplorerLeaf = window.locator(
-		".workspace-leaf-content[data-type=trash-explorer]"
-	);
-
-	await window.getByLabel("Open trash explorer", { exact: true }).click();
-	await expect(trashExplorerLeaf).toBeVisible();
+	const view = await openTrashView(page);
 
 	const trashItemName = "Recipes";
-	const trashItem = trashExplorerLeaf.locator(".trash-item", {
-		hasText: trashItemName,
-	});
+	const trashItem = getTrashItems(view).filter({ hasText: trashItemName });
 	await expect(trashItem).toContainText("2 files, 0 folders");
+
 	await trashItem.getByLabel("Delete permanently").click();
 
-	await window.locator("button.mod-warning").getByText("Delete").click();
+	await page.locator("button.mod-warning").getByText("Delete").click();
 
-	await expect(window.locator(".notice")).toHaveText(
+	await expect(page.locator(".notice")).toHaveText(
 		`Permanently deleted "${trashItemName}"`
 	);
 	await expect(trashItem).not.toBeVisible();
 });
 
 test("can filter trashed items by name", async () => {
-	const window = await app.firstWindow();
-	const trashExplorerLeaf = window.locator(
-		".workspace-leaf-content[data-type=trash-explorer]"
-	);
+	const view = await openTrashView(page);
 
-	await window.getByLabel("Open trash explorer", { exact: true }).click();
-
-	const trashItems = trashExplorerLeaf.locator(".trash-item");
+	const trashItems = getTrashItems(view);
 	await expect(trashItems).toHaveCount(5);
 
-	const searchInput = trashExplorerLeaf.getByPlaceholder("Filter by name...");
+	const searchInput = view.getByPlaceholder("Filter by name...");
 	await searchInput.fill("fløde");
 
 	await expect(trashItems).toHaveCount(2);
@@ -134,3 +121,25 @@ test("can filter trashed items by name", async () => {
 	await searchInput.clear();
 	await expect(trashItems).toHaveCount(5);
 });
+
+function getTrashItems(locator: Locator) {
+	return locator.locator(".trash-item");
+}
+
+function getTrashView(page: Page) {
+	return page.locator(".workspace-leaf-content[data-type=trash-explorer]");
+}
+
+function getFilesView(page: Page) {
+	return page.locator(".workspace-leaf-content[data-type=file-explorer]");
+}
+
+async function openTrashView(page: Page) {
+	await page.getByLabel("Open trash explorer", { exact: true }).click();
+	return getTrashView(page);
+}
+
+async function openFilesView(page: Page) {
+	await page.getByLabel("Files", { exact: true }).click();
+	return getFilesView(page);
+}
